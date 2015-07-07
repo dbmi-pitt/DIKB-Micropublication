b11d5d3ead4dcac151d1d336be05d123a340275c
Richard Boyce <ningyifan1212@163.com>
2015-07-07 14:20:20 -0400

    revise benchmark script - run against multiple graphs

--- scripts/queryTesting.py
+++ scripts/queryTesting.py
@@ -1,9 +1,31 @@
+## READ ALL QUERIES FROM FOLDER query/, EXECUTE ALL QUERIES UPON MP GRAPH WITH DIFFERENT FOLDS
+ 
+## Authors:
+## Yifan Ning
+
 import sys, os, datetime
+import time
 sys.path.append('.')
 from SPARQLWrapper import SPARQLWrapper, JSON
+from datetime import datetime
+
+################################################################################
+# Globals
+################################################################################
+QUERIES_PATH = "../queries/benchmark-queries/"
+
+#sparql = SPARQLWrapper("http://dbmi-icode-01.dbmi.pitt.edu/sparql")
+#sparql = SPARQLWrapper("https://dbmi-icode-01.dbmi.pitt.edu/sparql")
+sparql = SPARQLWrapper("http://localhost:8890/sparql")
 
 
-QUERIES_PATH = "../queries/"
+
+################################################################################
+# Functions
+################################################################################
+
+def getGraphDict():
+    return {"name":None, "fold":None, "cost":None, "triples":None}
 
 def readCSVfromDir(inputdir):
     queriesD = {}
@@ -17,32 +39,79 @@ def readCSVfromDir(inputdir):
     return queriesD
 
 
+def readGraphConfig():
+
+    graphD = dict(line.strip().split('=') for line in open('graph-config.properties'))
+    return graphD
+
+
+def runQuery(qry, graphname):
+
+    #try:
+    qry = qry.replace("graph",graphname)
+
+    # print "[INFO] testing query on graph (%s): \n %s" % (graphname, qry)
+
+    st1 = time.time()
+    
+    sparql.setQuery(qry)    
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+
+    st2 = time.time()
+    st3 = st2 - st1
+
+    if len(results["results"]["bindings"]) == 0:
+        print "[DEBUG] Query is not getting any results back"
+    else:
+        print "[INFO] Query return %s results \n time cost: %s" % (len(results["results"]["bindings"]), str(st3))
+
+    return str(st3)
+    #except:
+    #    print "[DEBUG] Query is not a correct query or not getting any results back"
+    
+    
+
+def runAllQueries(queryFolder):
+    
+    queriesD = readCSVfromDir(queryFolder)
+
+    sparql.method = 'POST'
+
+    for qryName,qry in queriesD.items():
+
+        if qry.strip() == "":
+            #print "[WARN] skip empty query %s" % qryName
+            continue
+
+        print "[QUERY] %s" % qryName
+        
+        analysisD = {}
+
+        ## run query on graphs with different folds
+        graphsD = readGraphConfig()
+
+        print graphsD
+        
+        for k,graphName in graphsD.items():
+            
+            analysisD[graphName] = {}
+            
+            cost = runQuery(qry, graphName)
+            
+            analysisD[graphName]["fold"] = k
+            analysisD[graphName]["cost"] = cost
+
+        print analysisD
+        print "\n"
+            
+
 ################################################################################
 ## MAIN TESTING
 ################################################################################
 
-queriesD = readCSVfromDir(QUERIES_PATH)
 
-#sparql = SPARQLWrapper("http://dbmi-icode-01.dbmi.pitt.edu/sparql")
-sparql = SPARQLWrapper("https://dbmi-icode-01.dbmi.pitt.edu/sparql")
-#sparql = SPARQLWrapper("http://localhost:8890/sparql")
-
-sparql.method = 'POST'
-
-for name,qry in queriesD.items():
+if __name__ == "__main__":
+    readGraphConfig()
+    runAllQueries(QUERIES_PATH)
     
- #   print "[INFO] testing query (%s): \n %s" % (name, qry)
-
-    try:
-
-        sparql.setQuery(qry)    
-        sparql.setReturnFormat(JSON)
-        results = sparql.query().convert()
-    
-        if len(results["results"]["bindings"]) == 0:
-            print "[WARN] Query : %s is not getting any results back" % (name) 
-        else:
-            print "[INFO] Query : %s returns %s results" % (name, len(results["results"]["bindings"])) 
-
-    except:
-        print "[DEBUG] Query %s is not a correct query or not getting any results back" % (name)
